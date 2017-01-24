b6b733661fc6ecfe82bcc8a20bd269a03472966a
kevinburnside <kevinburnside15@hotmail.com>
2017-01-24 13:32:09 -0500

    second push

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //index
+let express = require('express')
+let app = express()
+
+app.get('/', function(req, res){
+    res.send('Hello World')
+})
+
+app.listen(3000, function(){
+    console.log('Example app')
+})
